9e545a0f87e5cc4a344c85e789be6cb692a5f11b
Abubakar <abubakarzahid895@gmail.com>
2025-11-17 18:04:25 +0500

    made backend serverless

---
+++ BACKEND/api/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { dbConnection } from "../src/db/dbConection.js";
+import { app } from "../src/app.js";
+import { Route } from "../src/Router/userRouter.js";
+import { TodoRoute } from "../src/Router/TodoRouter.js";
+import { EfficiencyRoute } from "../src/Router/EfficiencyRouter.js";
+
+const response = dbConnection();
+
+app.use("/users", Route);
+app.use("/todos", TodoRoute);
+app.use("/eficiency", EfficiencyRoute);
+
+app.get("/", (req, res) => {
+  res.send("ok", response);
+});
+
+export default app;
--- BACKEND/src/db/dbConection.js
+++ BACKEND/src/db/dbConection.js
@@ -4,7 +4,9 @@ async function dbConnection() {
   try {
     await mongoose.connect(process.env.DB_URL);
     console.log("✅ MongoDB connected successfully");
+    return "✅ MongoDB connected successfully"
   } catch (error) {
+    return "❌ MongoDB connection failed:"
     console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1); 
   }
